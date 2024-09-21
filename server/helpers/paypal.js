cfcdfac2efbd0f7b507d15285fb9f25539e350ec
NitishMishra0 <nitishmishrani@gmail.com>
2024-09-22 02:13:16 +0530

    added all changes

---
+++ server/helpers/paypal.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const paypal = require("paypal-rest-sdk");
+
+paypal.configure({
+  mode: process.env.PAYPAL_MODE,
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_SECRET,
+});
+
+module.exports = paypal;
